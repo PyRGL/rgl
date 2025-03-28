c6c6e1f40d6db5219e3bfe04a320a2d4b16c56b1
liyuan <e0403849@u.nus.edu>
2025-03-28 21:18:34 +0800

    fix setup.py

--- clibs/build_linux.sh
+++ clibs/build_linux.sh
@@ -5,14 +5,20 @@
 
 # # rely on python version
 # c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) retrieve.cpp -o libretrieval$(python3-config --extension-suffix)
+#!/bin/bash
 
-for py_ver in 3.7 3.8 3.9 3.10 3.11 3.12 3.13; do
-    # conda create -n build_py$py_ver python=$py_ver -y
-    pypath=~/miniconda3/envs/build_py$py_ver/bin/python3
-    pyconfig=~/miniconda3/envs/build_py$py_ver/bin/python3-config
-    $pypath -m pip install pybind11
-    c++ -O3 -Wall -shared -std=c++11 -fPIC $($pypath -m pybind11 --includes) retrieve.cpp -o libretrieval$($pyconfig --extension-suffix)
+versions=(3.7 3.8 3.9 3.10 3.11 3.12 3.13)
+# for py_ver in "${versions[@]}"; do
+#     conda create -n build_py${py_ver} python=${py_ver} -y
+# done
+# for py_ver in "${versions[@]}"; do
+#     pypath=~/miniconda3/envs/build_py${py_ver}/bin/python3
+#     pyconfig=~/miniconda3/envs/build_py${py_ver}/bin/python3-config
+#     $pypath -m pip install pybind11
+#     c++ -O3 -Wall -shared -std=c++11 -fPIC $($pypath -m pybind11 --includes) retrieve.cpp -o libretrieval$($pyconfig --extension-suffix)
+# done
+for py_ver in "${versions[@]}"; do
+    conda env remove -n build_py${py_ver} -y
 done
-
 
 mv *.so ../rgl/graph_retrieval/
--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ import setuptools
 import glob
 from setuptools import setup, find_packages
 
-packages = find_packages(exclude=["benchmarks", "data", "demo", "dist", "doc", "docs", "logs", "models", "test"])
+packages = find_packages(
+    exclude=["benchmarks", "clibs", "data", "demo", "dist", "doc", "docs", "logs", "models", "test"]
+)
 print("packages:", packages)
 
 # Find all matching files
@@ -21,7 +23,7 @@ setup(
     author_email="e0403849@u.nus.edu",
     packages=packages,
     install_requires=["ogb", "patool", "faiss-cpu"],
-    extras_require={"torch-2.4": ["dgl @ https://data.dgl.ai/wheels/torch-2.4/dgl-0.9.0-cp38-cp38-linux_x86_64.whl"]},
+    # extras_require={"torch-2.4": ["dgl @ https://data.dgl.ai/wheels/torch-2.4/dgl-0.9.0-cp38-cp38-linux_x86_64.whl"]},
     package_data={"rgl": relative_files},
     description="RGL - RAG-on-Graphs Library",
     license="GNU General Public License v3.0 (See LICENSE)",
