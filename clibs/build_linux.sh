4a3106a49b0f4030b7538ecd285b49851e065cbc
liyuan <e0403849@u.nus.edu>
2025-03-28 17:59:00 +0800

    compile all pybind

--- clibs/build_linux.sh
+++ clibs/build_linux.sh
@@ -3,5 +3,16 @@
 
 # g++ retrieve.cpp -o retrieve
 
-c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) retrieve.cpp -o libretrieval$(python3-config --extension-suffix)
+# # rely on python version
+# c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) retrieve.cpp -o libretrieval$(python3-config --extension-suffix)
+
+for py_ver in 3.7 3.8 3.9 3.10 3.11 3.12 3.13; do
+    # conda create -n build_py$py_ver python=$py_ver -y
+    pypath=~/miniconda3/envs/build_py$py_ver/bin/python3
+    pyconfig=~/miniconda3/envs/build_py$py_ver/bin/python3-config
+    $pypath -m pip install pybind11
+    c++ -O3 -Wall -shared -std=c++11 -fPIC $($pypath -m pybind11 --includes) retrieve.cpp -o libretrieval$($pyconfig --extension-suffix)
+done
+
+
 mv *.so ../rgl/graph_retrieval/
