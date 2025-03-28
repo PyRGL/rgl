05d8a81dbb2a846dd2b47280aa0ededf1e72a8d7
liyuan <e0403849@u.nus.edu>
2025-03-28 18:05:49 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 import setuptools
-import io
+import glob
 from setuptools import setup, find_packages
 
 packages = find_packages(exclude=["benchmarks", "data", "demo", "dist", "doc", "docs", "logs", "models", "test"])
 print("packages:", packages)
+
+# Find all matching files
+# This returns a list of file paths like "rgl/graph_retrieval/libretrieval.cpython-310-x86_64-linux-gnu.so"
+package_files = glob.glob("rgl/graph_retrieval/libretrieval.cpython-3*-x86_64-linux-gnu.so")
+
+# Adjust paths to be relative to the package directory ("rgl").
+# That is, remove the leading "rgl/" portion.
+relative_files = [f[len("rgl/") :] for f in package_files]
 
 setup(
     name="rgl",
@@ -14,7 +22,7 @@ setup(
     packages=packages,
     install_requires=["ogb", "patool", "faiss-cpu"],
     extras_require={"torch-2.4": ["dgl @ https://data.dgl.ai/wheels/torch-2.4/dgl-0.9.0-cp38-cp38-linux_x86_64.whl"]},
-    package_data={"rgl": ["graph_retrieval/libretrieval.cpython-310-x86_64-linux-gnu.so"]},
+    package_data={"rgl": relative_files},
     description="RGL - RAG-on-Graphs Library",
     license="GNU General Public License v3.0 (See LICENSE)",
     long_description="RGL - RAG-on-Graphs Library",
